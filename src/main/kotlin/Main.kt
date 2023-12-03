bdf6a71a7636f0a4af506259835a8f3324653fc9
Lukasz Kalnik <lukasz.kalnik@gmail.com>
2023-12-03 19:03:23 +0100

    Part 2: imperative digit word parser

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,14 +2,42 @@ import okio.FileSystem
 import okio.Path.Companion.toPath
 
 fun main(args: Array<String>) {
-    val input = FileSystem.SYSTEM.read("input.txt".toPath()) {readUtf8()}.dropLast(1).split("\n")
+    val input = FileSystem.SYSTEM.read("input.txt".toPath()) { readUtf8() }.dropLast(1).split("\n")
 
-   val sumOfCalibrationValues = input.sumOf { line ->
-        val firstDigit = line.asIterable().first { it.isDigit() }
-        val lastDigit = line.asIterable().last { it.isDigit() }
-        val numberString = "$firstDigit$lastDigit"
-        numberString.toInt()
+    input.forEach { line ->
+        val digits = line.asDigits()
+
+        println(digits)
+    }
+}
+
+val digitWords = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+
+fun String.asDigits(): List<Char> {
+    val digits = mutableListOf<Char>()
+
+    for (i in indices) {
+        if (this[i].isDigit()) {
+            digits += this[i]
+            continue
+        }
+
+        findDigitWord(i, digits)
     }
 
-    println(sumOfCalibrationValues)
+    return digits
+}
+
+private fun String.findDigitWord(i: Int, digits: MutableList<Char>) {
+    for (digitWordIndexed in digitWords.withIndex()) {
+        val digitWord = digitWordIndexed.value
+        val substringLength = digitWord.length
+
+        if (i + substringLength <= length &&
+            substring(i, i + substringLength) == digitWord
+        ) {
+            digits += (digitWordIndexed.index + 1).toString().asIterable().first()
+            break
+        }
+    }
 }
