57f4dd2c4b6bc622ef542ec0c8287b353b18b424
Lukasz Kalnik <lukasz.kalnik@gmail.com>
2023-12-04 18:59:03 +0100

    Solve part 2 by only parsing first and last digits

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,8 +5,9 @@ fun main(args: Array<String>) {
     val input = FileSystem.SYSTEM.read("input.txt".toPath()) { readUtf8() }.dropLast(1).split("\n")
 
     val sumOfCoordinates = input.sumOf { line ->
-        val digits = line.asDigits()
-        digits.first() * 10 + digits.last()
+        val firstDigit = line.firstDigit(digitWords)
+        val lastDigit = line.reversed().firstDigit(digitWords.map { it.reversed() })
+        firstDigit * 10 + lastDigit
     }
 
     println(sumOfCoordinates)
@@ -14,31 +15,18 @@ fun main(args: Array<String>) {
 
 val digitWords = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
 
-fun String.asDigits(): List<Int> {
-    val digits = mutableListOf<Int>()
-
+fun String.firstDigit(digitWords: List<String>): Int {
     for (i in indices) {
-        if (this[i].isDigit()) {
-            digits += this[i].digitToInt()
-            continue
-        }
-
-        findDigitWord(i, digits)
+        if (this[i].isDigit()) return this[i].digitToInt()
+        findDigitWord(i, digitWords)?.let { return it }
     }
-
-    return digits
+    return 0 // never happens with input data
 }
 
-private fun String.findDigitWord(i: Int, digits: MutableList<Int>) {
+private fun String.findDigitWord(i: Int, digitWords: List<String>): Int? {
     for (digitWordIndexed in digitWords.withIndex()) {
         val digitWord = digitWordIndexed.value
-        val substringLength = digitWord.length
-
-        if (i + substringLength <= length &&
-            substring(i, i + substringLength) == digitWord
-        ) {
-            digits += (digitWordIndexed.index + 1)
-            break
-        }
+        if (substring(i).startsWith(digitWord)) return digitWordIndexed.index + 1
     }
+    return null
 }
